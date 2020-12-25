2f0399db859b32e7a2ba5d7bd0af63e3cfd2299f
mete <metekaba96@gmail.com>
2020-12-25 18:23:42 +0300

    Some UI improvements have been done.

--- TomarBlogUI/Models/ContactViewModels/SendEmailViewModel.cs
+++ TomarBlogUI/Models/ContactViewModels/SendEmailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,17 @@ namespace TomarBlogUI.Models.ContactViewModels
     public class SendEmailViewModel
     {
         public string To { get; set; }
+        [Display(Name="Let your name be known:")]
         public string GuestFirstName { get; set; }
+        [Display(Name ="Your surname:")]
         public string GuestLastName { get; set; }
+        [Display(Name = "May I get your number down here?")]
         public string GuestPhoneNumber { get; set; }
+        [Display(Name = "Email please:")]
         public string GuestEmailAddress { get; set; }
+        [Display(Name = "Briefly:")]
         public string Subject { get; set; }
+        [Display(Name = "What is it about?:")]
         public string Body { get; set; }
     }
 }
