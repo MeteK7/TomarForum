c40f805fcd78065ae623fee4bc213940d651fdbf
mete <metekaba96@gmail.com>
2021-02-01 20:29:07 +0300

    The development of the PostBusinessManager has been started.

--- TomarUI/BusinessManagers/Interfaces/IPostBusinessManager.cs
+++ TomarUI/BusinessManagers/Interfaces/IPostBusinessManager.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using TomarDAL.Entities;
+using TomarUI.Models.HomeViewModels;
+using TomarUI.Models.PostViewModels;
+using TomarUI.ViewModels.PostViewModels;
 
 namespace TomarUI.BusinessManagers.Interfaces
 {
     public interface IPostBusinessManager
     {
+        IndexViewModel GetIndexViewModel(string searchString, int? page);
+        Task<ActionResult<PostViewModel>> GetPostViewModel(int? id, ClaimsPrincipal claimsPrincipal);
+        Task<Post> CreatePost(CreateViewModel createViewModel, ClaimsPrincipal claimsPrincipal);
+        Task<ActionResult<Comment>> CreateComment(PostViewModel postViewModel, ClaimsPrincipal claimsPrincipal);
+        Task<ActionResult<EditViewModel>> UpdatePost(EditViewModel editViewModel, ClaimsPrincipal claimsPrincipal);
+        Task<ActionResult<EditViewModel>> GetEditViewModel(int? id, ClaimsPrincipal claimsPrincipal);
     }
 }
