6174684bc3172e3f780482df4c36e88a1626275e
mete <metekaba96@gmail.com>
2021-02-02 17:14:08 +0300

    Register View has been improved.

--- TomarUI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TomarUI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -61,6 +61,14 @@ namespace TomarUI.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
