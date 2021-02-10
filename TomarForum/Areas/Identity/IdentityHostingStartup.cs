bcb466a73213559f45b583484928afd63c268484
mete <metekaba96@gmail.com>
2021-02-11 02:22:43 +0300

    Added identity.

---
+++ TomarForum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TomarForum.Data;
+
+[assembly: HostingStartup(typeof(TomarForumUI.Areas.Identity.IdentityHostingStartup))]
+namespace TomarForumUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
