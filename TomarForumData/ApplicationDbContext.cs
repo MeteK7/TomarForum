8198d3eabbdb4fcde8ee634cd736362c30a31a5a
mete <metekaba96@gmail.com>
2021-02-10 22:12:10 +0300

    Two classes named ApplicationDbContext and ApplicationUser have been added.

---
+++ TomarForumData/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomarForumData.Models;
+
+namespace TomarForumData
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ TomarForumData/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomarForumData.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string UserDescription { get; set; } //Change the order of the properties later in order to be organized.
+        public string ProfileImageUrl { get; set; }
+        public int Rating { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime MemberSince { get; set; }
+    }
+}
+
+//It is necessary to add property to the class named "ApplicationUser" for every data related with the user.
