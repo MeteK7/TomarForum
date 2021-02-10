0155a5f9fe87692475ed168172454a15b020d7e7
mete <metekaba96@gmail.com>
2021-02-10 21:24:12 +0300

    EmailSender has been added.

---
+++ TomarForumServices/EmailSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomarForumServices.Interfaces;
+
+namespace TomarForumServices
+{
+    public class EmailSender:IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
