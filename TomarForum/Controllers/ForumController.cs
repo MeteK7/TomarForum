f098110550d46e7ccbb3feefa3d63bd21c757872
mete <metekaba96@gmail.com>
2021-02-12 22:01:46 +0300

    Dependency Injection for ForumService in ForumController.

---
+++ TomarForum/Controllers/ForumController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TomarForumData.EntityModels;
+using TomarForumService.Interfaces;
+
+namespace TomarForumUI.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForumService _forumService;
+        public ForumController(IForumService forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Forum> forums = _forumService.GetAll();
+            return View();
+        }
+    }
+}
