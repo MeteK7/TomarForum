cba36c656bd2b4a5ec026e6270db945626f6aa81
mete <metekaba96@gmail.com>
2021-02-14 20:40:46 +0300

    Created a button in Topic.cshtml to create a new Post.

---
+++ TomarForumData/Migrations/20210214171714_ChangedColumns.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TomarForumData.Migrations
+{
+    public partial class ChangedColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Posts",
+                newName: "DateCreated");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "PostReplies",
+                newName: "DateCreated");
+
+            migrationBuilder.RenameColumn(
+                name: "MemberSince",
+                table: "AspNetUsers",
+                newName: "MembershipCreatedOn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "Posts",
+                newName: "Created");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "PostReplies",
+                newName: "Created");
+
+            migrationBuilder.RenameColumn(
+                name: "MembershipCreatedOn",
+                table: "AspNetUsers",
+                newName: "MemberSince");
+        }
+    }
+}
