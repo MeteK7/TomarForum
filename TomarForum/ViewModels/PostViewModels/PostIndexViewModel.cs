e992382f34dedaad0b9a4c8b5c0f54d07d8c8680
mete <metekaba96@gmail.com>
2021-02-16 21:49:21 +0300

    PostReplyViewModel and PostIndexViewModel have now a new property.

--- TomarForum/ViewModels/PostViewModels/PostIndexViewModel.cs
+++ TomarForum/ViewModels/PostViewModels/PostIndexViewModel.cs
@@ -14,6 +14,7 @@ namespace TomarForumUI.ViewModels.PostViewModels
         public string AuthorName { get; set; }
         public string AuthorImageUrl { get; set; }
         public int AuthorRating { get; set; }
+        public bool IsAuthorAdmin { get; set; }
         public DateTime DateCreated { get; set; }
         public string PostContent { get; set; }
         public int ForumId { get; set; }
