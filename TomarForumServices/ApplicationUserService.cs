46aa5757a11fd8696aa643c8a09d564ddfc7798b
mete <metekaba96@gmail.com>
2021-02-18 18:38:28 +0300

    ApplicationUserService's methods are developed.

--- TomarForumServices/ApplicationUserService.cs
+++ TomarForumServices/ApplicationUserService.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TomarForumData;
+using TomarForumData.EntityModels;
 using TomarForumService.Interfaces;
 
 namespace TomarForumService
 {
-    public class ApplicationUserService:IApplicationUserService
+    public class ApplicationUserService : IApplicationUserService
     {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public ApplicationUserService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            return _applicationDbContext.ApplicationUsers;
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return GetAll().FirstOrDefault(user => user.Id == id);
+        }
+
+        public Task IncrementRating(string id, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetProfileImage(string id, Uri uri)
+        {
+            var user = GetById(id);
+            user.ProfileImageUrl = uri.AbsoluteUri;
+            _applicationDbContext.Update(user);
+            await _applicationDbContext.SaveChangesAsync();
+        }
     }
 }
--- TomarForumServices/Interfaces/IApplicationUserService.cs
+++ TomarForumServices/Interfaces/IApplicationUserService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TomarForumData.EntityModels;
 
 namespace TomarForumService.Interfaces
 {
     public interface IApplicationUserService
     {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+        Task SetProfileImage(string id, Uri uri);
+        Task IncrementRating(string id, Type type);
+
     }
 }
