a7572710d16e0acd62087d85a2f217d330d19fdb
mete <metekaba96@gmail.com>
2021-03-09 17:58:57 +0300

    HomeContactViewModel has been composed.

--- TomarForumViewModel/HomeViewModels/HomeContactViewModel.cs
+++ TomarForumViewModel/HomeViewModels/HomeContactViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,20 @@ namespace TomarForumViewModel.HomeViewModels
 {
     public class HomeContactViewModel
     {
+        public string To { get; set; }
+        [Display(Name = "Your name:")]
+        public string GuestFirstName { get; set; }
+        [Display(Name = "Your surname:")]
+        public string GuestLastName { get; set; }
+        [Display(Name = "Phone number:")]
+        public string GuestPhoneNumber { get; set; }
+        [Display(Name = "Email:")]
+        public string GuestEmailAddress { get; set; }
+        [Display(Name = "Subject:")]
+        public string Subject { get; set; }
+        [Display(Name = "Body:")]
+        public string Body { get; set; }
+        [Display(Name = "Attachment:")]
+        public List<IFormFile> Attachment { get; set; }
     }
 }
